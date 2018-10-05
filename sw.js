e3ad40ed32c7fbb283c3292ec38682d9221d29af
Adrian Wix <adrianwix18@gmail.com>
2018-10-05 22:44:05 +0200

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-15bd8ec3680c819a3136.js"
+    "url": "webpack-runtime-e05910459fe4b6b330db.js"
   },
   {
     "url": "app-9f67343a71b7e50ab5dc.js"
@@ -36,11 +36,11 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "4faf8ce6113a1b01e10b16fd5c973235"
+    "revision": "2df5d673f96a0c5c1f825551545ce004"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "61e34c265cfc817c59d4f9599e3d2aa8"
+    "revision": "0ac8656bc430c734df332ee64c84c4d1"
   },
   {
     "url": "0.24b72a9c0d6e77774d8e.css"
@@ -49,7 +49,7 @@ self.__precacheManifest = [
     "url": "1.d3fc6456ea4804455645.css"
   },
   {
-    "url": "1-2a3d97ff223af4061710.js"
+    "url": "1-507c2f7c19c080e8c40e.js"
   },
   {
     "url": "component---src-pages-index-js-b35ca9743091a174d99e.js"
